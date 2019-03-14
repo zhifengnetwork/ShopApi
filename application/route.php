4e8f04cc4fcdc9cdf6bba10c020072aad265b370
aixil123 <934068103@qq.com>
2019-03-14 20:59:23 +0800

    路由配置注册登录

--- application/route.php
+++ application/route.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 use think\Route;
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
@@ -25,7 +25,11 @@ Route::get('/',function(){
     return '<h1>商城接口</h1>';
 });
 
-
 Route::any('cart/get_cart_list','Cart/cart/get_cart_list',['method'=>'get|post']);
+ 
+Route::get('user/login','user/Login/Login');
+Route::get('user/reg','user/Reg/reg');
+
+//Route::get('user/cs','user/Cs/ss');
 
 

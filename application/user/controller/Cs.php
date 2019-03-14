86c72d659a253eb72e6e17a65a07bf5fe0ace40f
aixil123 <934068103@qq.com>
2019-03-14 20:55:26 +0800

    用户登录注册接口

---
+++ application/user/controller/Cs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\user\controller;use think\Controller;
+
+
+
+class Cs extends Controller
+{
+    public function ss()
+    {
+        
+        
+        return $this->fetch();
+        
+    }
+    
+}

82c7b3e98da4a02e77b7eb802028f730b5869b50
FZJ <yudwcom@163.com>
2019-03-16 12:33:23 +0800

    图片

--- application/upload/controller/Test.php
+++ application/upload/controller/Test.php
@@ -7,12 +7,18 @@ class Test
     {
         $data = file_get_contents("php://input");
 
+        if(!$data || empty($data)){
+            exit(json_encode('status'=>-1,'msg'=>'错误，内容为空'));
+        }
+
         $filename = time().'.png'; 
         
         if(file_put_contents($filename,$data)){ 
-            echo 'success'; 
+         
+            exit(json_encode('status'=>-1,'msg'=>'保存成功'));
         }else{ 
-            echo 'failed'; 
+         
+            exit(json_encode('status'=>-1,'msg'=>'保存失败'));
         } 
 
     }

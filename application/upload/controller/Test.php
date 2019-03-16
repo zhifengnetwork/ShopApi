c44e1dbe7607653e62b17ec15fd821f0475bcf52
FZJ <yudwcom@163.com>
2019-03-16 14:04:56 +0800

    图片upload

--- application/upload/controller/Test.php
+++ application/upload/controller/Test.php
@@ -5,20 +5,23 @@ class Test
 {
     public function index()
     {
+
+        header('Content-type: application/json');
+
         $data = file_get_contents("php://input");
 
         if(!$data || empty($data)){
-            exit(json_encode('status'=>-1,'msg'=>'错误，内容为空'));
+            exit(json_encode(['status'=>-1,'msg'=>'错误，内容为空'],JSON_UNESCAPED_UNICODE));
         }
 
         $filename = time().'.png'; 
         
         if(file_put_contents($filename,$data)){ 
          
-            exit(json_encode('status'=>-1,'msg'=>'保存成功'));
+            exit(json_encode(['status'=>-1,'msg'=>'保存成功'],JSON_UNESCAPED_UNICODE));
         }else{ 
          
-            exit(json_encode('status'=>-1,'msg'=>'保存失败'));
+            exit(json_encode(['status'=>-1,'msg'=>'保存失败'],JSON_UNESCAPED_UNICODE));
         } 
 
     }

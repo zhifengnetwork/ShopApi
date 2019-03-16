68ef561a0976d0f148c6a6350982848e9ca9a0ee
aixil123 <934068103@qq.com>
2019-03-16 20:11:55 +0800

    账号登录与注册

--- application/user/controller/Login.php
+++ application/user/controller/Login.php
@@ -10,30 +10,46 @@ class Login extends Controller
     {
         
         $mobile = Request::instance()->post('mobile');
-        $password = Request::instance()->post('password');
-        $password1 = md5('TPSHOP'.$password);
+        $password =Request::instance()->post('password');
+       $password1 = md5('TPSHOP'.$password);
 
-        $res = Db::name('users')->where(['mobile'=>$mobile,'password'=>$password1])->find();
-     
-        if(!$res){
+        $res = Db::name('users')->where(['mobile'=>$mobile])->find();
+        $res2=Db::name('users')->where(['password'=>$password1])->find();
+        if($res==FALSE){
             $data = array(
                'status' => -1,
                 'msg' => '用户不存在'
             );
             ajaxReturn($data);
             exit;
+        }elseif ($res&&$res2==FALSE){
+            $data = array(
+                'status' => -2,
+                'msg' => '密码错误'
+            );
+            ajaxReturn($data);
+            exit;
+        }elseif ($res&&$res2){
+            
+            $res['token'] = md5(time()).md5(time());
+            
+            Db::name('users')->where(['mobile'=>$mobile])->update(['token'=>$res['token']]);
+            
+            $data = array(
+                'status' => 1,
+                'msg' => '登录成功',
+                'data' => $res
+            );
+            ajaxReturn($data);
+            exit;
+            
+            
         }
         
-        $res['token'] = md5(time()).md5(time());
-    
-        Db::name('users')->where(['mobile'=>$mobile])->update(['token'=>$res['token']]);
-     
-        $data = array(
-            'status' => 1,
-            'msg' => '请求成功',
-            'data' => $res
-        );
-        ajaxReturn($data);
-        exit;
+        
+        
+        
+        
+       
     }
 }
